374cb869c83ce646c5b4f0b647ad94316df87597
姜玉鹏 <jiangyp@gantsoftware.com>
2019-09-04 14:44:08 +0800

    - [x] 删除边 aql 语句优化 - [x] 扩展表不同步到 arangodb 中

--- doc/md_relation_metadata.sql
+++ doc/md_relation_metadata.sql
@@ -7,7 +7,7 @@ SELECT tc.constraint_name,
 FROM information_schema.table_constraints AS tc
          JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
          JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
-WHERE constraint_type = 'FOREIGN KEY';
+WHERE constraint_type = 'FOREIGN KEY' and ccu.table_name ~ '.*(?<!_ext)$'; -- 不是以 ‘_ext' 结尾的约束
 
 -- 创建关系表
 drop table mstdata.md_relation_metadata;
@@ -20,6 +20,10 @@ CREATE TABLE if not exists mstdata.md_relation_metadata
     to_table        text,
     to_column       text
 );
+
+-- 还需要一个唯一键 from_table + from_column + to_table + to_column
+-- 需要一个触发器
+
 -- 插入数据
 insert into mstdata.md_relation_metadata(constraint_name, from_table, from_column, to_table, to_column)
 SELECT tc.constraint_name,
@@ -30,11 +34,8 @@ SELECT tc.constraint_name,
 FROM information_schema.table_constraints AS tc
          JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
          JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
-WHERE constraint_type = 'FOREIGN KEY';
+WHERE constraint_type = 'FOREIGN KEY' and ccu.table_name ~ '.*(?<!_ext)$';
 
 -- 查询
 select *
 from mstdata.md_relation_metadata mrm;
-
--- 还需要一个唯一键 from_table + from_column + to_table + to_column
--- 需要一个触发器
